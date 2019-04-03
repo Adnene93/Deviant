eb60adb5a703c325ce62a36dd9ef051851b5be1f
Adnene BELFODIL <adnene.belfodil@insa-lyon.fr>
2019-04-03 13:16:22 +0200

    updating XP-Q1

---
+++ Experiments_Scripts_and_Results/Q1-Taylor-vs-Empirical/Results/test_summary.bat
@@ -0,0 +1,6 @@
+python ../../../Deviant-Code/main.py results_CHUS_0.05_empirical_taylor_approx.csv -f --taylorapprox --first_run
+python ../../../Deviant-Code/main.py results_epd8_significant_consensual_0.05_empirical_taylor_approx.csv -f --taylorapprox 
+python ../../../Deviant-Code/main.py results_movielens_0.05_empirical_taylor_approx.csv -f --taylorapprox 
+python ../../../Deviant-Code/main.py results_yelp_0.05_empirical_taylor_approx.csv -f --taylorapprox 
+
+pause
---
+++ Experiments_Scripts_and_Results/Q1-Taylor-vs-Empirical/Scripts/execute_error_empirical_study.sh
@@ -0,0 +1,4 @@
+python ../../../Deviant-Code/main.py quality_MOVIELENS_PVALUE.json -q --compute_empirical_distribution --significance 0.05 --nb_random_sample 10000 ;
+python ../../../Deviant-Code/main.py quality_EPD8_SIGNIFICANT.json -q --compute_empirical_distribution --significance 0.05 --nb_random_sample 10000 ;
+python ../../../Deviant-Code/main.py quality_CHUS_PVALUE.json -q --compute_empirical_distribution --significance 0.05 --nb_random_sample 10000 ;
+python ../../../Deviant-Code/main.py quality_YELP_PVALUE.json -q --compute_empirical_distribution --significance 0.05 --nb_random_sample 10000 ;
